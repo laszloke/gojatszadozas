25ac8e51027248970ae5fff2be4aa0f33588d4d8
laszloke <laszloke@gmail.com>
2022-11-24 17:10:36 +0200

    go version display

--- ofic_web_tutorials/static/main.go
+++ ofic_web_tutorials/static/main.go
@@ -3,16 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"runtime"
 )
 
 func main() {
     http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Welcome to my website!")
+       fmt.Fprintf(w, "Welcome to my website!")
+       fmt.Fprintf(w,"Go version: %s\n", runtime.Version())
     })
 
     fs := http.FileServer(http.Dir("static/"))
     fs=http.StripPrefix("/static/", fs)
 	http.Handle("/static/", fs)
 
-    http.ListenAndServe(":80", nil)
+    http.ListenAndServe(":8080", nil)
 }
