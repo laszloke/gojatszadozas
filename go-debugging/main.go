4bf165710094a4f67ccddb80cacfdfef7708207d
laszloke <laszloke@gmail.com>
2022-11-24 12:43:54 +0200

    debug test programocska es vscode debug config

---
+++ go-debugging/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+   "encoding/json"
+   "fmt"
+   "log"
+)
+
+
+type user struct {
+   FullName string `json:"full_name"`
+   Email string `json:"email"`
+   Gender   string `json:"gender"`
+   Status   string `json:"status"`
+   RegDate   string `json:"Reg_date"` 
+}
+
+
+
+func main() {
+   userinfos := []user{
+       {
+           FullName: "blessing james",
+           Email: "blessing@gmail.com",
+           Gender:   "Male",
+           Status:   "active",
+           RegDate:"20-01-2021",
+       },
+       {
+           FullName: "matt john",
+           Email: "matt@gmail.com",
+           Gender:   "Male",
+           Status:   "active",
+           RegDate:"20-01-2021",
+       },
+       {
+           FullName: "john peace",
+           Email: "peace@gmail.com",
+           Gender:   "Midgard",
+           Status:   "active",
+           RegDate:"20-01-2021",
+       },
+   }
+
+
+   jsonBytes, err := json.Marshal(userinfos)
+   if err != nil {
+       log.Fatalln(err)
+   }
+   fmt.Println(string(jsonBytes))
+}
